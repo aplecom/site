81066984a6bc5af00da0c22ef8f647fdd0ad8d69
Ilya <novastacer@gmail.com>
2024-04-25 20:38:24 +0400

    register simpl tag for categories

--- app1/main/views.py
+++ app1/main/views.py
@@ -5,12 +5,9 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
-
     context = {
         'title':'Главная',
         'content':'Аренда селькохозяйственных комбайнов',
-        'categories': categories
     }
     return render(request,'main/index.html',context)
 

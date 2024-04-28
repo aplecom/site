1afd5bd4fcdacd6ee6248994233cadeda588e87a
Ilya <novastacer@gmail.com>
2024-04-28 13:16:08 +0400

    connect bd-> PostgreSQL

--- app1/app1/settings.py
+++ app1/app1/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'app1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'combine',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- app1/goods/utils.py
+++ app1/goods/utils.py
@@ -11,5 +11,6 @@ def q_search(query):
 
     for token in keywords:
         q_objects |= Q(description__icontains=token)
+        q_objects |= Q(name__icontains=token)
     
     return  Products.objects.filter(q_objects)

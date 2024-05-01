d8eeac8fd4304b790f74d0678060fffdf1642858
Ilya <novastacer@gmail.com>
2024-05-01 20:16:43 +0400

    carts 15%

--- app1/users/urls.py
+++ app1/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('',views.login,name = 'login'),
     path('registration/',views.registration,name = 'registration'),
     path('profile/',views.profile,name = 'profile'),
+    path('users-cart/', views.users_cart, name='users_cart'),
     path('logout/',views.logout,name = 'logout'),
 ]

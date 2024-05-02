dcadb5fe72b93a771c5e204c24b2e5e7b7b73979
Ilya <novastacer@gmail.com>
2024-05-02 05:01:59 +0400

    Проверка на номер

--- app1/orders/forms.py
+++ app1/orders/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 class CreateOrderForm(forms.Form):
@@ -6,3 +7,15 @@ class CreateOrderForm(forms.Form):
     delivery_address = forms.CharField()
     land_area = forms.DecimalField()
     land_processing_date = forms.DateField()
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+        
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
